29ea900049640f76a47a93120a451af2a38c227d
Aniket Yadav <84137048+aryan216@users.noreply.github.com>
2024-07-17 17:31:56 +0000

    route snippet added

---
+++ src/App2.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import React, { lazy,Suspense} from "react";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Home = lazy(() => import("./components/Home"));
+
+function App2() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navigate />
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={"loading.."}>
+                <Dashboard />{" "}
+              </Suspense>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"loading.."}>
+                <Home />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+function Navigate() {
+  const Navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          Navigate("/");
+        }}
+      >
+        Home
+      </button>
+      <button
+        onClick={() => {
+          Navigate("/dashboard");
+        }}
+      >
+        Dashboard
+      </button>
+    </div>
+  );
+}
+
+export default App2;
